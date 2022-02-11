1eb081ce25a3f0b9e5f8d0d72c3b5f0d211902bf
jpanackal <jpanackal@wisc.edu>
2022-02-11 16:08:13 -0600

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         return(
@@ -19,23 +19,29 @@ import { Card, CardImg, CardText, CardBody,
 
     function RenderComments({comments}) {
 
-        const dishComment = comments.map((comment) => {
+        if (comments != null) {
             return (
-                <div>
-                    <div key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
-                </div>
-            );
-        });
-
-        return(
                 <div className='col-12 col-md-5 m-1'>
                     <h4>Comments</h4>
-                    {dishComment}
+                    <ul className='"list-unstyled'>
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+    
 
     }
 
@@ -43,9 +49,17 @@ import { Card, CardImg, CardText, CardBody,
         if (props.dish != null) {
             return (
                 <div className='container'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
                     <div className='row'>
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
